81f60309ae79477e03bd328c70d55f6656b4127d
Sean <>
2021-09-29 13:05:56 -0700

    first

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9bf8cc74ab83590b280df0851beaec60'),
+    },
+  },
+});
