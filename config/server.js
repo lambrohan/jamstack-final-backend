09e800b91adae8be623d45886511c5d20cd73995
Sean Modd <sean@senpex.com>
2021-10-03 05:15:33 +0000

    Updated server config for NGINX

--- config/server.js
+++ config/server.js
@@ -1,6 +1,8 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  cron: { enabled: true },
+  url: env("URL", "http://localhost"),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '9bf8cc74ab83590b280df0851beaec60'),
