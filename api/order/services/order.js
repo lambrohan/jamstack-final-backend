11d428529702cd064d19ccfea313269b8f35b81a
Sean <>
2021-10-13 12:56:21 -0700

    fixed server.js... LOOK BACK AT api/order/services/order.js because I changed my localhost:1337 perhaps incorrectly here for deployment not sure...

--- api/order/services/order.js
+++ api/order/services/order.js
@@ -244,7 +244,7 @@ module.exports = {
 
                                             <tbody><tr>
                                               <td class="imgAuto"  style= "font-family:'Poppins', sans-serif;margin-top: 0px;margin-bottom: 0px;text-align: left;padding-left: 8px;padding-right: 8px;">
-                                                            <img src="localhost:1337${
+                                                            <img src="admin.shopcarx.com${
                                                               item.variant
                                                                 .images[0].url
                                                             }" width="90" style="width: 90px;" alt="${
