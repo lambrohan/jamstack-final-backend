23a8108583717ba95fb2cfc655f13327bf5f0220
Sean <>
2021-10-13 13:22:11 -0700

    changed to the suggestion from StackOverflow

--- config/server.js
+++ config/server.js
@@ -1,14 +1,30 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  cron: { enabled: true },
-  url: env('URL', 'http://localhost'),
+// module.exports = ({ env }) => ({
+//   host: env('HOST', '0.0.0.0'),
+//   port: env.int('PORT', 80),
+//   cron: { enabled: true },
+//   url: env('URL', 'http://localhost'),
 
 
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '9bf8cc74ab83590b280df0851beaec60'),
-    },
-  },
+//   admin: {
+//     auth: {
+//       secret: env('ADMIN_JWT_SECRET', '9bf8cc74ab83590b280df0851beaec60'),
+//     },
+//   },
 
-});
+// });
+
+module.exports = ({ env }) => {
+  const port = env('PORT', '1337');
+  const host = env('HOST', '0.0.0.0');
+  const url = env('URL', `http://localhost${port !== '80' ? ':'+port : ''}`);
+  const adminAuthSecret = env('ADMIN_JWT_SECRET', '9bf8cc74ab83590b280df0851beaec60');
+
+  return {
+    host, port, url,
+    cron: { enabled: true },
+    cors: { enabled: true, origin: ['*'] },
+    admin: {
+      auth: { secret: adminAuthSecret },
+    }
+  };
+};
