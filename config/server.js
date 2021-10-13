37bbe91fbb5d4a8adaa2ff8d89e4a4a06543f868
Sean <>
2021-10-13 16:00:53 -0700

    made changes to config/server.js and package.json

--- config/server.js
+++ config/server.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   host: env('HOST', '127.0.0.1'),
   port: env.int('PORT', 10080),
   cron: { enabled: true },
-//  url: env('URL', 'http://localhost'),
-   admin: {
-     auth: {
-       secret: env('ADMIN_JWT_SECRET', '9bf8cc74ab83590b280df0851beaec60'),
-     },
-   },
+  admin: {
+    url:  '/admin',
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9bf8cc74ab83590b280df0851beaec60'),
+    },
+  },
 
 });
